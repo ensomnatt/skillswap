9368a21d107f21f9e6adfa7f2bb513c6c4a64c07
ensomnatt <ensomnatt@protonmail.com>
2025-08-18 16:33:23 +0300

    add prisma

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const logger = new Logger("Bootstrap");
+
+  const port = configService.get<number>("PORT") || 3001;
+
+  await app.listen(port);
+
+  logger.log(`server running on http://localhost:${port}`);
 }
 bootstrap();
