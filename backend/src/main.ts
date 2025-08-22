02b8824098d05664db1ebe55c2367ec950204415
ensomnatt <ensomnatt@protonmail.com>
2025-08-22 19:00:13 +0300

    last changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || "3001";
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>("PORT") || "3001";
 
   app.enableCors({
     origin: "http://localhost:3000",
