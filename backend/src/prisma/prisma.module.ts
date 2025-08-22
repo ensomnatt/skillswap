3511df8ed0d0eeb2ca27b36738988c7f6b14b546
ensomnatt <ensomnatt@protonmail.com>
2025-08-22 16:28:49 +0300

    delete backend

--- backend/src/prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Global, Module } from "@nestjs/common";
-import { PrismaService } from "./service/prisma.service";
-
-@Global()
-@Module({
-  providers: [PrismaService]
-})
-export class PrismaModule { }
